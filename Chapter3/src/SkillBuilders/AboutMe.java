0959a45b8d3a94e69480b785a93daca334925c71
511026006 <511026006@L815WD220019.edu.cbe.ab.ca>
2025-11-26 09:17:06 -0700

    9:16am

---
+++ Chapter3/src/SkillBuilders/AboutMe.java
@@ -0,0 +1,25 @@
+/*
+
+Program: Bingo rules. java        		Date: Nov 26 2025
+
+Purpose: Create an AboutMe application that displays your first name and last initial
+
+
+Author: Montasr Mohamed
+School: CHHS
+Course: Computer Science 10
+ 
+
+*/
+
+package SkillBuilders;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("Montasr M.");                       // First name + last initial
+        System.out.println("Instructor: Mr. Abdulla");          // Instructor's name
+        System.out.println("School: Crescent Heights High School"); // School name
+
+        System.out.println("\"Go Cowboys!\""); // You can change this cheer if your school uses another one
+    }
+}
---
+++ Chapter3/src/SkillBuilders/Greeting.java
@@ -0,0 +1,12 @@
+package SkillBuilders;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello! Welcome to my Greeting Application.");
+        System.out.println("I hope you are having a great day!");
+    }
+}
+
+
+
+
